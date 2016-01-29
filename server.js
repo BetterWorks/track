55793426e83d2de93742b0b725907551d38cb93d
Jason Spafford <nullprogrammer@gmail.com>
2016-01-29 01:11:39 -0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,38 @@
+var http      = require('http');
+var httpProxy = require('http-proxy');
+var url       = require('url');
+
+var target   = process.env.TARGET || 'http://www.google.com:80'
+var port     = process.env.PORT || 8080;
+var siteFile = require('./apple-app-site-association.json');
+
+var proxy    = httpProxy.createProxyServer({'target': target, 'changeOrigin': true});
+var server   = http.createServer(onRequest).listen(port);
+
+proxy.on('error', onProxyError);
+
+function onRequest(req, res) {
+  path = url.parse(req.url).path
+
+  if(path == '/apple-app-site-association') {
+    respondWithJSON(res, siteFile);
+  } else {
+    console.log('Proxying request to ', path)
+    proxy.web(req, res);
+  }
+}
+
+function onProxyError(error, req, res) {
+  errorMessage = 'Error during proxy: ' + error
+  console.error(errorMessage)
+  res.writeHead(500, {'Content-Type': 'text/plain'});
+  res.end(errorMessage);
+}
+
+function respondWithJSON(res, value) {
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.write(JSON.stringify(value));
+  res.end();
+}
+
+console.log('Proxying requests on port ' + port + ' to ' + target)
