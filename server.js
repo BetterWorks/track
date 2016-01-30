81024fe2e32d3879df0d9364b2424c7b18336cf7
Di Wu <dw323@cornell.edu>
2016-01-29 20:25:25 -0800

    new relic

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
+if (process.env.NEW_RELIC_LICENSE_KEY) {
+  require('newrelic');
+}
+
+
 var http      = require('http');
 var httpProxy = require('http-proxy');
 var url       = require('url');
 
-var target   = process.env.TARGET || 'http://www.google.com:80'
+var target   = process.env.TARGET || 'http://mandrillapp.com';
 var port     = process.env.PORT || 8080;
 var siteFile = require('./apple-app-site-association.json');
 
@@ -12,19 +17,19 @@ var server   = http.createServer(onRequest).listen(port);
 proxy.on('error', onProxyError);
 
 function onRequest(req, res) {
-  path = url.parse(req.url).path
+  path = url.parse(req.url).path;
 
   if(path == '/apple-app-site-association') {
     respondWithJSON(res, siteFile);
   } else {
-    console.log('Proxying request to ', path)
+    console.log('Proxying request to ', path);
     proxy.web(req, res);
   }
 }
 
 function onProxyError(error, req, res) {
-  errorMessage = 'Error during proxy: ' + error
-  console.error(errorMessage)
+  errorMessage = 'Error during proxy: ' + error;
+  console.error(errorMessage);
   res.writeHead(500, {'Content-Type': 'text/plain'});
   res.end(errorMessage);
 }
@@ -35,4 +40,4 @@ function respondWithJSON(res, value) {
   res.end();
 }
 
-console.log('Proxying requests on port ' + port + ' to ' + target)
+console.log('Proxying requests on port ' + port + ' to ' + target);
